bc5814853dc76cd942f22f758c00acd161a680de
VTVinicius <53088125+VTVinicius@users.noreply.github.com>
2021-12-08 14:55:33 -0300

    feature: Feito o esboço das telas de home e pesquisar usuario

--- app/src/main/java/com/example/githubapplication/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/githubapplication/ui/dashboard/DashboardFragment.kt
@@ -4,17 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.githubapplication.R
-import com.example.githubapplication.databinding.FragmentDashboardBinding
+import com.example.githubapplication.databinding.FragmentSearchBinding
 
 class DashboardFragment : Fragment() {
 
     private lateinit var dashboardViewModel: DashboardViewModel
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentSearchBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -28,13 +25,9 @@ class DashboardFragment : Fragment() {
         dashboardViewModel =
             ViewModelProvider(this).get(DashboardViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
         return root
     }
 
--- app/src/main/java/com/example/githubapplication/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/githubapplication/ui/notifications/NotificationsFragment.kt
@@ -9,12 +9,12 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.githubapplication.R
-import com.example.githubapplication.databinding.FragmentNotificationsBinding
+import com.example.githubapplication.databinding.FragmentHistoryBinding
 
 class NotificationsFragment : Fragment() {
 
     private lateinit var notificationsViewModel: NotificationsViewModel
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentHistoryBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -28,7 +28,7 @@ class NotificationsFragment : Fragment() {
         notificationsViewModel =
             ViewModelProvider(this).get(NotificationsViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentHistoryBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textNotifications
