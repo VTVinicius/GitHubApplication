670335852dd059cffe588648706f6feb9503367f
VTVinicius <53088125+VTVinicius@users.noreply.github.com>
2021-12-19 12:23:18 -0300

    feature: criado modulos e inicio da configuração dos modulos

--- feature_search/src/main/java/com/example/feature_search/commom/navigation/FeatureSearchNavigation.kt
+++ feature_search/src/main/java/com/example/feature_search/commom/navigation/FeatureSearchNavigation.kt
@@ -1,4 +1,5 @@
 package com.example.feature_search.commom.navigation
 
 interface FeatureSearchNavigation {
+
 }
---
+++ feature_search/src/main/java/com/example/feature_search/search_user/SearchUserFagment.kt
@@ -0,0 +1,38 @@
+package com.example.feature_search.search_user
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.githubapplication.databinding.FragmentSearchBinding
+
+class SearchUserFagment : Fragment() {
+
+    private lateinit var searchUserViewModel: SearchUserViewModel
+    private var _binding: FragmentSearchBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        searchUserViewModel =
+            ViewModelProvider(this).get(SearchUserViewModel::class.java)
+
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
