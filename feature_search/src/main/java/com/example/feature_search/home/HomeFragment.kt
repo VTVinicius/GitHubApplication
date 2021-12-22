2b4925070ae9111d81c9984072168f0de2771e94
VTVinicius <53088125+VTVinicius@users.noreply.github.com>
2021-12-22 10:24:10 -0300

    feature: subindo alteraçoes

--- feature_search/src/main/java/com/example/feature_search/home/HomeFragment.kt
+++ feature_search/src/main/java/com/example/feature_search/home/HomeFragment.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.githubapplication.databinding.FragmentHomeBinding
+import com.example.feature_search.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
