a323ac9dd90b9ac50b661dcd52ca03baec5671fd
VTVinicius <53088125+VTVinicius@users.noreply.github.com>
2022-01-19 10:57:17 -0300

    feature: GithubRepository Tests Complete

---
+++ data/src/test/java/com/example/data/factory/GithubFactory.kt
@@ -0,0 +1,22 @@
+package com.example.data.factory
+
+import com.example.domain.model.github.GitUserDataModel
+import com.example.domain.model.github.GitUserModel
+import com.example.domain.model.github.SearchUserModel
+
+object GithubFactory {
+    val DUMMY_SEARCH_USER_MODEL = SearchUserModel(
+        id = 1,
+        login = "login",
+        avatar_url = "url",
+        bio = "bio",
+        email = "email",
+        name = "name"
+    )
+
+
+    val DUMMY_LIST_GIT_USER_MODEL = listOf(GitUserModel(gitUserData = GitUserDataModel(user = DUMMY_SEARCH_USER_MODEL)))
+
+    val DUMMY_GIT_USER_MODEL = GitUserModel(gitUserData = GitUserDataModel(user = DUMMY_SEARCH_USER_MODEL))
+
+}
